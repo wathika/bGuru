76caed9934896d1fad0ab17548645cf2707689e1
wathika <vcwathika@gmail.com>
2016-01-26 15:52:19 +0300

    initialized express html render

--- a/index.js
+++ a/index.js
@@ -1,9 +1,12 @@
 var root = require('root');
 var github = require('github-auth');
+var express = require('express');
+var path = require('path');
+
 var app = root();
 
 
-var gh = github('56d3b718497c147e3828', 'f10210d9e68b38bed3fea2c86bb727c58b8a932a', {
+var gh = github('2a900d9e9871c1b4c27f', '450c8bd04e582370a1de97206268c6aa3e07840b', {
   users: ['wathika']
 });
 
@@ -19,9 +22,29 @@ app.all('*', function(req, res, next) {
 });
 
 
-app.get('/', function(req, res) {
-  res.send('<h2>Hello World!</h2>');
+// app.get('/', function(req, res) {
+//   res.send('<h2>Hello World!</h2>');
+// });
+
+// app.get('/render', function(req, res) {
+//     res.render('login', {title: 'res vs app render'})
+// })
+
+
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+//
+// app.get('/', function(req, res, next){
+//     res.render('./login.html');
+// });
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/login.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
+// app.get('/', function(req, res) {
+//   res.render('login.html');
+// });
 
 app.listen(3000);
