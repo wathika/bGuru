ce102f398195fa59ddec8831e9587da1d0811106
wathika <vcwathika@gmail.com>
2016-01-26 15:57:13 +0300

    develop web server which delivers HTML files like how your apache do

--- expressRenderHtml/server.js
+++ expressRenderHtml/server.js
@@ -1,21 +1,24 @@
 var express = require("express");
 var app     = express();
-var path    = require("path");
-
+app.use(express.static(__dirname + '/View'));
+//Store all HTML files in view folder.
+app.use(express.static(__dirname + '/Script'));
+//Store all JS and CSS in Scripts folder.
 
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+  res.sendFile('index.html');
+  //It will find and locate index.html from View or Scripts
 });
 
 app.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
+  res.sendFile('/about.html');
 });
 
 app.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
+  res.sendFile('/sitemap.html');
 });
 
 app.listen(3000);
 
 console.log("Running at Port 3000");
+
